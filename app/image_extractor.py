89313fb15865174496d3e873a3e6d7d5f7e9eeca
dor <harpazdor@gmail.com>
2023-10-08 17:05:56 +0000

    Iterate videos

--- app/image_extractor.py
+++ app/image_extractor.py
@@ -3,61 +3,49 @@ from collections import namedtuple
 
 import cv2
 import numpy as np
+import filetype as ft
 
 FileKey = str
 FileContent = bytes
 
 ImageType = namedtuple("ImageType", [
-	"image",
-	"source_file_extension",
-	"source_key",
-	"file_index",
-	"image_index_in_file",
+    "image",
+    "source_file_extension",
+    "source_file_type",
+    "source_key",
+    "file_index",
+    "image_index_in_file",
 ])
 
 def iterate_images(files_iterator: Iterable[Tuple[FileKey, FileContent]]):
-	for file_index, (file_key, file_content) in enumerate(files_iterator):
+    for file_index, (file_key, file_content) in enumerate(files_iterator):
         print(f"[*] Processing Image {file_index:4d}")
 
         file_extension = obj['Key'].split('.')[-1].lower()
 
-		if file_extension == "jpg":
+        file_kind = ft.guess(file_content)
+
+        if file_extension == "jpg":
             image_np_array = np.frombuffer(file_content, np.uint8)
             image = cv2.imdecode(image_np_array, cv2.IMREAD_COLOR)
-			yield ImageType(image, file_extension, file_key, file_index, 0)
+            yield ImageType(image, file_extension, file_kind, file_key, file_index, 0)
 
-		elif file_extension == "mp4":
-			yield from extract_images_from_video(...)
+        elif file_extension == "mp4":
+            with tempfile.NamedTemporaryFile(suffix=".mp4") as temp_file:
+                temp_file.write(file_content_bytes)
+                temp_file.seek(0)
 
-def old_code():	
-    images = []
-    for file in files:
-        dir, path, mime, filename = file.values()
+                # Open the video using cv2.VideoCapture
+                video_capture = cv2.VideoCapture(temp_file.name, cv2.CAP_FFMPEG)
 
-        if mime is None:
-            continue
-        if mime.startswith('video'):
-            print('[INFO] extracting frames from video...')
-            video = cv2.VideoCapture(path)
-            while True:
-                success, frame = video.read()
-                if success and isinstance(frame, np.ndarray):
-                    image = {
-                        "file": frame,
-                        "sourcePath": path,
-                        "sourceType": "video",
-                        "filename": filename
-                    }
-                    images.append(image)
-                else:
-                    break
-            video.release()
-            cv2.destroyAllWindows()
-        elif mime.startswith('image'):
-            image = {
-                "file": cv2.imread(path),
-                "sourcePath": path,
-                "sourceType": "image",
-                "filename": filename
-            }
-            images.append(image)
+                frame_index = 0
+                while True:
+                    ret, frame = video_capture.read()
+                    if not ret:
+                        break
+
+                    yield ImageType(frame, file_extension, file_kind, file_key, file_index, frame_index)
+                    frame_index += 1
+
+                video_capture.release()
+                cv2.destroyAllWindows()
